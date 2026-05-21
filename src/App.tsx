693eb734c978d8a2ff94703c3de69337a58150e4
Daria Borisiak <daria.borisiak@gmail.com>
2026-05-21 20:33:26 +0200

    Load wind turbine model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { Canvas } from '@react-three/fiber'
+
+import { Experience } from './Experience/Experience'
 import './App.css'
 
 function App() {
   return (
-    <>
-      Hello, world!
-    </>
+      <Canvas
+          shadows
+          camera={ {
+              fov: 45,
+              near: 0.1,
+              far: 200,
+              position: [ - 8, 8, 10 ]
+          } }
+      >
+          <Experience />
+      </Canvas>
   )
 }
 
---
+++ src/Experience/Experience.tsx
@@ -0,0 +1,26 @@
+import { OrbitControls, Stage } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { Suspense } from 'react'
+
+import { WindTurbine } from './WindTurbine'
+
+export function Experience() {
+    return (
+        <>
+            <Perf position="top-left" />
+
+            <OrbitControls makeDefault />
+
+            <Suspense fallback={
+                <mesh position-y={0.5} scale={[2, 3, 2]}>
+                    <boxGeometry args={[1, 1, 1, 2, 2, 2]}/>
+                    <meshBasicMaterial wireframe color="red"/>
+                </mesh>
+            }>
+                <Stage adjustCamera environment="city" shadows="contact">
+                    <WindTurbine />
+                </Stage>
+            </Suspense>
+        </>
+    )
+}
