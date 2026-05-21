e209169563f60cd2790dd88419a122e27d7d881d
Daria Borisiak <daria.borisiak@gmail.com>
2026-05-21 23:40:10 +0200

    Fix shadows

--- src/Experience/Experience.tsx
+++ src/Experience/Experience.tsx
@@ -10,7 +10,20 @@ export function Experience() {
 
             <OrbitControls makeDefault />
 
-            <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } shadow-normalBias={0.04}/>
+            <directionalLight
+                castShadow
+                position={ [ 1, 2, 3 ] }
+                intensity={ 4.5 }
+                shadow-normalBias={0.04}
+                shadow-camera-left={-15}
+                shadow-camera-right={15}
+                shadow-camera-top={15}
+                shadow-camera-bottom={-15}
+                shadow-camera-near={-20}
+                shadow-camera-far={20}
+                shadow-radius={10}
+                shadow-mapSize={[2048, 2048]}
+            />
             <ambientLight intensity={ 1.5 } />
 
             <mesh receiveShadow position-y={ 0} rotation-x={ - Math.PI * 0.5 } scale={ 20 }>
