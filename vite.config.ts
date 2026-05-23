3db7ef6945e58214b15d4b673635fda632b35e97
Daria Borisiak <daria.borisiak@gmail.com>
2026-05-23 17:53:49 +0200

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wind/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
