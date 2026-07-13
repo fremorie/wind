beac45dabbd4531d6cee9b6f116717fdc93a214d
Daria Borisiak <daria.borisiak@gmail.com>
2026-07-13 15:59:00 +0200

    Add tree model

--- src/Experience/Experience.tsx
+++ src/Experience/Experience.tsx
@@ -9,12 +9,22 @@ import { Cow } from '../components/Cow';
 import { WaterSurface } from '../components/WaterSurface';
 import { ConstructionSign } from '../components/constructionSign';
 import { Bushes } from '../components/Bushes';
+import { TreeModel } from '../components/Trees/TreeModel';
+import { CHUNK_SIZE, GRID_SIZE_Z } from '../utils/constants';
 
 export function Experience() {
     return (
         <>
             {/*<OrbitControls makeDefault />*/}
             <Environment />
+
+            <TreeModel
+                position={[
+                    ((GRID_SIZE_Z - 1) * CHUNK_SIZE) / 2 + 28,
+                    0,
+                    ((GRID_SIZE_Z - 1) * CHUNK_SIZE) / 2 + 24,
+                ]}
+            />
 
             <axesHelper />
 
