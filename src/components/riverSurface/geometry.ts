4d40dea7885e66c0337e24c6cd4829fb282fec9e
Daria Borisiak <daria.borisiak@gmail.com>
2026-08-29 14:45:01 +0200

    Tune river surface geometry

--- src/components/riverSurface/geometry.ts
+++ src/components/riverSurface/geometry.ts
@@ -3,10 +3,10 @@ import * as THREE from 'three';
 import { GRID_TOTAL_WIDTH } from '../../utils/constants';
 
 export const riverSurfaceGeometry = new THREE.PlaneGeometry(
-    GRID_TOTAL_WIDTH,
-    GRID_TOTAL_WIDTH,
-    16,
-    16,
+    GRID_TOTAL_WIDTH * 0.75,
+    GRID_TOTAL_WIDTH * 0.75,
+    8,
+    8,
 );
 
 riverSurfaceGeometry.rotateX(-Math.PI / 2);
